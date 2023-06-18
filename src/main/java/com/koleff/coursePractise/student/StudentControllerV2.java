6534d256e43d5c7016c49db180de69c322241c7e
Koleff <martin_koleff@abv.bg>
2023-06-18 22:54:30 +0300

    Added PUT and DELETE Rest methods. Added API versioning and deprecated v1.

--- src/main/java/com/koleff/coursePractise/student/StudentControllerV2.java
+++ src/main/java/com/koleff/coursePractise/student/StudentControllerV2.java
@@ -6,14 +6,13 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "api/v1/student")
-@Deprecated
-public class StudentController {
+@RequestMapping(path = "api/v2/student")
+public class StudentControllerV2 {
 
     private final StudentService studentService;
 
     @Autowired
-    public StudentController(StudentService studentService) {
+    public StudentControllerV2(StudentService studentService) {
         this.studentService = studentService;
     }
 
@@ -24,19 +23,19 @@ public class StudentController {
 
     @PostMapping
     void createNewStudent(@RequestBody Student student){
-        System.out.println("POST REQUEST RECEIVED!");
+        System.out.println("POST REQUEST V2 RECEIVED!");
         System.out.println("Student -> " + student);
     }
 
     @PutMapping
     void updateStudent(@RequestBody Student student){
-        System.out.println("UPDATE REQUEST RECEIVED!");
+        System.out.println("UPDATE REQUEST V2 RECEIVED!");
         System.out.println("Student -> " + student);
     }
 
     @DeleteMapping(path = "{studentId}")
     void deleteStudent(@PathVariable("studentId") Long id){
-        System.out.println("DELETE REQUEST RECEIVED!");
+        System.out.println("DELETE REQUEST V2 RECEIVED!");
         System.out.println("Student deleted with id -> " + id);
     }
 }

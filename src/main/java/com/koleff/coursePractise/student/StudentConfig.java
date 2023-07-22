e173db5b38cc65981b34be26893e7b871d3e5aee
MartinKoleff <martin_koleff@abv.bg>
2023-07-22 13:41:15 +0300

    Added old repositories imports in studentConfig

--- src/main/java/com/koleff/coursePractise/student/StudentConfig.java
+++ src/main/java/com/koleff/coursePractise/student/StudentConfig.java
@@ -1,5 +1,7 @@
 package com.koleff.coursePractise.student;
 
+import com.koleff.coursePractise.student.fakeRepository.StudentFakeRepo;
+import com.koleff.coursePractise.student.fakeRepository.StudentFakeRepoImpl;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;

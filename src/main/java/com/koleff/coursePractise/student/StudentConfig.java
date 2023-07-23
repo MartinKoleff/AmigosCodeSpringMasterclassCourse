e60bab73f49fe209a63a293356a6b4a9d2f4a160
MartinKoleff <martin_koleff@abv.bg>
2023-07-23 19:43:04 +0300

    Added two example ways to use application.properties data

--- src/main/java/com/koleff/coursePractise/student/StudentConfig.java
+++ src/main/java/com/koleff/coursePractise/student/StudentConfig.java
@@ -2,10 +2,12 @@ package com.koleff.coursePractise.student;
 
 import com.koleff.coursePractise.student.fakeRepository.StudentFakeRepo;
 import com.koleff.coursePractise.student.fakeRepository.StudentFakeRepoImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class StudentConfig {
@@ -13,11 +15,18 @@ public class StudentConfig {
     @Value("${app.useFakeStudentRepository:false}")
     private Boolean useFakeStudentRepository;
 
+    @Value("${info.company.name}")
+    private String companyName;
+
+    @Autowired //inject in constructor...
+    private Environment environment;
 
     @Bean
     CommandLineRunner commandLineRunner() {
         return args -> {
             System.out.println("Command line runner...");
+            System.out.println(companyName);
+            System.out.println(environment.getProperty("info.app.version"));
         };
     }
 
